91932374ff29b977f9ab4fc258864dc7227b740c
Kevin <kevinponting82@gmail.com>
2023-11-05 15:13:02 -0800

    step 7 complete

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 const USERNAME = 'ADMIN_EMAIL'
 const PASSWORD = 'ADMIN_PASSWORD'
 
+import PocketBase from "https://cdnjs.cloudflare.com/ajax/libs/pocketbase/0.19.0/pocketbase.es.js"
+
 const pb = new PocketBase('http://127.0.0.1:8090')
 
 const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD)
